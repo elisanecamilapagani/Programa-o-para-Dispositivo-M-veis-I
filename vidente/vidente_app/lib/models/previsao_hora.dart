e27f4f9ec7e61a17818351872b86b97113db924a
Elisane Pagani <elisanecamila20@gmail.com>
2021-05-07 00:10:47 -0400

    commit inicial

---
+++ vidente/vidente_app/lib/models/previsao_hora.dart
@@ -0,0 +1,13 @@
+class PrevisaoHora {
+  final String horario;
+  final String descricao;
+  final String numeroIcone;
+  final String temperatura;
+
+  PrevisaoHora(
+    this.horario,
+    this.descricao,
+    this.numeroIcone,
+    this.temperatura,
+  );
+}
