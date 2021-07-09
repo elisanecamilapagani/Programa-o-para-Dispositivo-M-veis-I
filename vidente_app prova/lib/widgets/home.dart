3f5dcf23f1e3ade89126fcfa7cffc89afe8edf02
Laís Matie <lais@jeraapps.com>
2021-07-09 00:42:00 -0400

    incluindo input e deixando a aplicação dinâmica

--- vidente_app prova/lib/widgets/home.dart
+++ vidente_app prova/lib/widgets/home.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:vidente_app/models/previsao_hora.dart';
-import 'package:vidente_app/services/previsao_service.dart';
+import 'package:vidente_app/services/previsao_service_dio.dart';
 import 'package:vidente_app/widgets/proximas_temperaturas.dart';
 import 'package:vidente_app/widgets/resumo.dart';
 
@@ -19,8 +19,8 @@ class _HomeState extends State<Home> {
   }
 
   void carregarPrevisoes() {
-    PrevisaoService service = PrevisaoService();
-    ultimasPrevisoes = service.recuperarUltimasPrevisoes();
+    PrevisaoServiceDio service = PrevisaoServiceDio();
+    ultimasPrevisoes = service.recuperarUltimasPrevisoes('Aquidauana');
   }
 
   Future<Null> atualizarPrevisoes() async {
@@ -36,50 +36,78 @@ class _HomeState extends State<Home> {
       ),
       body: RefreshIndicator(
         onRefresh: atualizarPrevisoes,
-        child: Center(
-          child: FutureBuilder<List<PrevisaoHora>>(
-            future: ultimasPrevisoes,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                List<PrevisaoHora> previsoes = snapshot.data;
-                double temperaturaAtual = previsoes[0].temperatura;
-                double menorTemperatura = double.maxFinite;
-                double maiorTemperatura = double.negativeInfinity;
-                previsoes.forEach((obj) {
-                  if (obj.temperatura < menorTemperatura) {
-                    menorTemperatura = obj.temperatura;
+        child: SingleChildScrollView(
+          child: SizedBox(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            child: Center(
+              child: FutureBuilder<List<PrevisaoHora>>(
+                future: ultimasPrevisoes,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    List<PrevisaoHora> previsoes = snapshot.data;
+                    double temperaturaAtual = previsoes[0].temperatura;
+                    double menorTemperatura = double.maxFinite;
+                    double maiorTemperatura = double.negativeInfinity;
+                    previsoes.forEach((obj) {
+                      if (obj.temperatura < menorTemperatura) {
+                        menorTemperatura = obj.temperatura;
+                      }
+
+                      if (obj.temperatura > maiorTemperatura) {
+                        maiorTemperatura = obj.temperatura;
+                      }
+                    });
+
+                    String descricao = previsoes[0].descricao;
+                    int numeroIcone = previsoes[0].numeroIcone;
+
+                    return Column(
+                      children: [
+                        Resumo(
+                          cidade: 'Aquidauana-MS',
+                          temperaturaAtual: temperaturaAtual,
+                          temperaturaMaxima: maiorTemperatura,
+                          temperaturaMinima: menorTemperatura,
+                          descricao: descricao,
+                          numeroIcone: numeroIcone,
+                        ),
+                        Padding(padding: EdgeInsets.all(10)),
+                        Container(
+                          width: 200,
+                          height: 100,
+                          child: TextField(
+                            style: TextStyle(
+                              color: Colors.black,
+                            ),
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              hintText: 'Digite sua cidade',
+                            ),
+                          ),
+                        ),
+                        FlatButton(
+                          // onPressed: () {
+                          //   Navigator.pop(context, cityName);
+                          // },
+                          child: Text(
+                            'Buscar Temperatura de sua cidade',
+                            // style: kButtonTextStyle,
+                          ),
+                        ),
+                        ProximasTemperaturas(
+                          previsoes: previsoes.sublist(1, previsoes.length),
+                        )
+                      ],
+                    );
+                  } else if (snapshot.hasError) {
+                    return Text('Erro ao carregar as previsões');
                   }
 
-                  if (obj.temperatura > maiorTemperatura) {
-                    maiorTemperatura = obj.temperatura;
-                  }
-                });
-
-                String descricao = previsoes[0].descricao;
-                int numeroIcone = previsoes[0].numeroIcone;
-
-                return Column(
-                  children: [
-                    Resumo(
-                      cidade: 'Aquidauana-MS',
-                      temperaturaAtual: temperaturaAtual,
-                      temperaturaMaxima: maiorTemperatura,
-                      temperaturaMinima: menorTemperatura,
-                      descricao: descricao,
-                      numeroIcone: numeroIcone,
-                    ),
-                    Padding(padding: EdgeInsets.all(10)),
-                    ProximasTemperaturas(
-                      previsoes: previsoes.sublist(1, previsoes.length),
-                    )
-                  ],
-                );
-              } else if (snapshot.hasError) {
-                return Text('Erro ao carregar as previsões');
-              }
-
-              return CircularProgressIndicator();
-            },
+                  return CircularProgressIndicator();
+                },
+              ),
+            ),
           ),
         ),
       ),
