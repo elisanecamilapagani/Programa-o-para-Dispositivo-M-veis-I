ab8403f0a78dab70fcb87e7730381e92279c1a16
elisane camila pagani <elisanecamila20@gmail.com>
2021-07-11 21:56:43 -0400

    ajustando 0.1

--- vidente_app prova/lib/utils/constants.dart
+++
@@ -1,5 +0,0 @@
-class Constants {
-  static const String baseUrl = 'http://dataservice.accuweather.com';
-  static const String cityPath = '$baseUrl/locations/v1/cities/search';
-  static const String weatherPath = '$baseUrl/forecasts/v1/hourly/12hour/{locationKey}';
-}
